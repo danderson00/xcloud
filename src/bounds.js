4945d9ce8e08180f27cb37a134697e72c8eaaa27
danderson00 <danderson00@gmail.com>
2017-08-15 21:01:49 +1000

    Fixed overlap test

--- src/bounds.js
+++ src/bounds.js
@@ -1,18 +1,22 @@
-module.exports = {
+var bounds = module.exports = {
   overlapping: function (a, b) {
-    return (Math.abs(2.0 * a.left + a.width - 2.0 * b.left - b.width) < a.width + b.width) ||
-        (Math.abs(2.0 * a.top + a.height - 2.0 * b.top - b.height) < a.height + b.height);
+    return between(a.left, b.left, b.width)
+      || between(b.left, a.left, a.width)
+      || between(a.top, b.top, b.height)
+      || between(b.top, a.top, a.height)
+
+    function between(value, min, increase) {
+      return value > min && value < min + increase
+    }
   },
 
   hitTest: function (newWord, existingWords) {
-    return existingWords.some(word => module.exports.overlapping(newWord, word))
+    return existingWords.some(word => bounds.overlapping(newWord, word))
   },
 
   outsideContainer: function (word, width, height) {
-    return (
-        word.left < 0 || word.top < 0 ||
-        (word.left + word.width) > width ||
-        (word.top + word.height) > height
-    )
+    return word.left < 0 || word.top < 0
+      || (word.left + word.width) > width
+      || (word.top + word.height) > height
   }
 }
--- test/bounds.test.js
+++ test/bounds.test.js
@@ -3,12 +3,12 @@ var bounds = require('../src/bounds')
 test("overlapping detects overlapping bounds", () => {
   expect(bounds.overlapping(
     { left: 0, top: 0, width: 10, height: 10 },
-    { left: 10, top: 0, width: 10, height: 10 }
+    { left: 11, top: 0, width: 10, height: 10 }
   )).toBe(false)
 
   expect(bounds.overlapping(
     { left: 0, top: 0, width: 10, height: 10 },
-    { left: 0, top: 10, width: 10, height: 10 }
+    { left: 0, top: 11, width: 10, height: 10 }
   )).toBe(false)
 
   expect(bounds.overlapping(
