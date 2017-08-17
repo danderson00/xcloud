a7aa686edd452d73ee2828d1afc8aff0ff04a711
danderson00 <danderson00@gmail.com>
2017-08-17 18:13:23 +1000

    Refactor

--- src/index.js
+++ src/index.js
@@ -4,55 +4,55 @@ const bounds = require('./bounds')
 const layout = require('./layout')
 
 const defaultOptions = {
-    width: 640,
-    height: 480,
-    steps: 10,
-    shape: 'elliptic',
-    removeOverflowing: true,
-    colors: { r: 34, g: 85, b: 153 },
-    fontSize: { from: 0.02, to: 0.07 },
-    font: 'Arial'
+  width: 640,
+  height: 480,
+  steps: 10,
+  shape: 'elliptic',
+  removeOverflowing: true,
+  colors: { r: 34, g: 85, b: 153 },
+  fontSize: { from: 0.02, to: 0.07 },
+  font: 'Arial'
 }
 
 module.exports = function(words, options) {
-    if(!options.measureText || options.measureText.constructor !== Function)
-        throw new Error('You must provide a measureText option!')
+  if(!options.measureText || options.measureText.constructor !== Function)
+    throw new Error('You must provide a measureText option!')
 
-    if(!words || words.length === 0)
-        return []
+  if(!words || words.length === 0)
+    return []
 
-    options = Object.assign(defaultOptions, options)
+  options = Object.assign(defaultOptions, options)
+  
+  words.forEach(word => word.weight = parseFloat(word.weight, 10))
+  words.sort((a, b) => b.weight - a.weight)
+
+  let outputWords = []
+  let maxWeight = words[0].weight
+  let minWeight = words[words.length - 1].weight
+  let sizes = fontSizes.generate(options.fontSize, options.steps, options.width, options.height)
+  let colors = colorGenerator.generate(options.colors, options.steps)
+
+  words.forEach((word, index) => layoutWord(index, word))
+
+  return outputWords
+  
+  function layoutWord(index, word) {
+    const weight = fontSizes.mapWeightToScale(word.weight, minWeight, maxWeight, options.steps)
+    const dimensions = options.measureText(word.text, options.font, sizes[weight - 1])
     
-    words.forEach(word => word.weight = parseFloat(word.weight, 10))
-    words.sort((a, b) => b.weight - a.weight)
+    let outputWord = layout.next(index, options, outputWords, {
+      color: colors[weight - 1],
+      size: sizes[weight - 1],
+      weight: weight,
+      text: word.text,
+      font: options.font,
+      width: dimensions.width,
+      height: dimensions.height,
+      left: (options.width - dimensions.width) / 2.0,
+      top: (options.height - dimensions.height) / 2.0
+    })
 
-    let outputWords = []
-    let maxWeight = words[0].weight
-    let minWeight = words[words.length - 1].weight
-    let sizes = fontSizes.generate(options.fontSize, options.steps, options.width, options.height)
-    let colors = colorGenerator.generate(options.colors, options.steps)
-
-    words.forEach((word, index) => layoutWord(index, word))
-
-    return outputWords
-    
-    function layoutWord(index, word) {
-        const weight = fontSizes.mapWeightToScale(word.weight, minWeight, maxWeight, options.steps)
-        const dimensions = options.measureText(word.text, options.font, sizes[weight - 1])
-        
-        let outputWord = layout.next(index, options, outputWords, {
-            color: colors[weight - 1],
-            size: sizes[weight - 1],
-            weight: weight,
-            text: word.text,
-            font: options.font,
-            width: dimensions.width,
-            height: dimensions.height,
-            left: (options.width - dimensions.width) / 2.0,
-            top: (options.height - dimensions.height) / 2.0
-        })
-
-        if (!(options.removeOverflowing && bounds.outsideContainer(outputWord, options.width, options.height)))
-            outputWords.push(outputWord)
-    }
+    if (!(options.removeOverflowing && bounds.outsideContainer(outputWord, options.width, options.height)))
+      outputWords.push(outputWord)
+  }
 }
--- src/layout.js
+++ src/layout.js
@@ -2,40 +2,9 @@ const bounds = require('./bounds')
 
 const layout = module.exports = {
   next: function (index, options, outputWords, outputWord) {
-      return layout[options.shape](index, outputWord, outputWords, options.width, options.height)
+    return layout[options.shape](index, outputWord, outputWords, options.width, options.height)
   },
-  rectangular: function (index, outputWord, outputWords, width, height) {
-    let stepsInDirection = 0.0
-    let quarterTurns = 0.0
-    let step = 18.0
-    let aspectRatio = width / height
-    
-    while (bounds.hitTest(outputWord, outputWords)) {
-      stepsInDirection++
-      
-      if (stepsInDirection * step > (1 + Math.floor(quarterTurns / 2.0)) * step * ((quarterTurns % 4 % 2) === 0 ? 1 : aspectRatio)) {
-          stepsInDirection = 0.0
-          quarterTurns++
-      }
-
-      switch (quarterTurns % 4) {
-          case 1:
-              outputWord.left += step * aspectRatio + Math.random() * 2.0
-              break
-          case 2:
-              outputWord.top -= step + Math.random() * 2.0
-              break
-          case 3:
-              outputWord.left -= step * aspectRatio + Math.random() * 2.0
-              break
-          case 0:
-              outputWord.top += step + Math.random() * 2.0
-              break
-      }
-    }
-
-    return outputWord     
-  },
+  
   elliptic: function (index, outputWord, outputWords, width, height) {
     let currentAngle = Math.random() * 6.28
     let radius = 0.0
@@ -51,5 +20,38 @@ const layout = module.exports = {
     }
     
     return outputWord
+  },
+
+  rectangular: function (index, outputWord, outputWords, width, height) {
+    let stepsInDirection = 0.0
+    let quarterTurns = 0.0
+    let step = 18.0
+    let aspectRatio = width / height
+    
+    while (bounds.hitTest(outputWord, outputWords)) {
+      stepsInDirection++
+      
+      if (stepsInDirection * step > (1 + Math.floor(quarterTurns / 2.0)) * step * ((quarterTurns % 4 % 2) === 0 ? 1 : aspectRatio)) {
+        stepsInDirection = 0.0
+        quarterTurns++
+      }
+
+      switch (quarterTurns % 4) {
+        case 1:
+          outputWord.left += step * aspectRatio + Math.random() * 2.0
+          break
+        case 2:
+          outputWord.top -= step + Math.random() * 2.0
+          break
+        case 3:
+          outputWord.left -= step * aspectRatio + Math.random() * 2.0
+          break
+        case 0:
+          outputWord.top += step + Math.random() * 2.0
+          break
+      }
+    }
+
+    return outputWord     
   }
 }
