7d020092aea90ecfb6840df73a3fdb7e6094dbc3
danderson00 <danderson00@gmail.com>
2017-08-17 18:14:26 +1000

    Refactor

--- src/bounds.js
+++ src/bounds.js
@@ -1,9 +1,9 @@
 var bounds = module.exports = {
   overlapping: function (a, b) {
-    return !(b.left > a.left + a.width || 
-      a.left > b.left + b.width || 
-      b.top > a.top + a.height || 
-      a.top > b.top + b.height)
+    return !(b.left > a.left + a.width 
+      || a.left > b.left + b.width
+      || b.top > a.top + a.height
+      || a.top > b.top + b.height)
   },
 
   hitTest: function (newWord, existingWords) {
@@ -11,7 +11,8 @@ var bounds = module.exports = {
   },
 
   outsideContainer: function (word, width, height) {
-    return word.left < 0 || word.top < 0
+    return word.left < 0 
+      || word.top < 0
       || (word.left + word.width) > width
       || (word.top + word.height) > height
   }
